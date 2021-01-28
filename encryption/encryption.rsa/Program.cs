a219616508d0c4bf6c9163c09a78507ccfa09a5c
wei-an.dong wei-an.dong <974608610@qq.com>
2021-01-28 14:09:35 +0800

    [main] [add] 添加读取 pem 文件生成 RSA 密钥

--- encryption/encryption.rsa/Program.cs
+++ encryption/encryption.rsa/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using encryption.rsa.github;
 
 namespace encryption.rsa
 {
@@ -16,15 +17,16 @@ namespace encryption.rsa
                 Console.Write($"加密内容：{input}\r\n");
                 var rsa = new RsaAlgorithm();
 
-                Console.WriteLine($"RSA私钥：\r\n{rsa.PrivateKey}\r\n");
+                Console.WriteLine($"RSA公钥：\r\n{rsa.PublicKey}\r\n");
                 var encrypt = rsa.Encrypt(input);
                 Console.WriteLine($"RSA加密后内容：\r\n{encrypt}\r\n");
                 var sign = rsa.Sign("SHA1", input);
-                Console.WriteLine($"RSA生成数字签名[SHAI]：\r\n{sign}\r\n");
-
-                Console.WriteLine($"RSA公钥：\r\n{rsa.PublicKey}\r\n");
+                
+                Console.WriteLine($"RSA私钥：\r\n{rsa.PrivateKey}\r\n");
                 var decrypt = rsa.Decrypt(encrypt);
                 Console.WriteLine($"RSA解密后内容：\r\n{decrypt}\r\n");
+
+                Console.WriteLine($"RSA生成数字签名[SHAI]：\r\n{sign}\r\n");
                 string signResult = rsa.VerifySign(decrypt, "SHA1", sign) ? "验签通过" : "验签未通过";
                 Console.WriteLine($"RSA进行鉴别数字签名：{signResult}");
             }
@@ -43,6 +45,36 @@ namespace encryption.rsa
                 var decrypt = rsa.Decrypt(encrypt);
                 Console.WriteLine($"RSA解密后内容：\r\n{Convert.ToBase64String(decrypt)}\r\n");
                 System.IO.File.WriteAllBytes("1.rar", decrypt);
+            }
+
+
+            {
+                Console.WriteLine("-----------------------------------------------------通过读取PEM文件 RSA 文件加密与解密--------------------------------------------------");
+                var input = "公钥密码体制中，目前最著名的是由美国三位科学家Rivest, Shamir 和 Adleman 于1976年提出，并在1978年正式发表的RSA 算法。";
+                Console.Write($"加密内容：{input}\r\n");
+                var rsa = new RsaAlgorithm();
+
+                rsa.CreateEncryptRSA(System.IO.File.ReadAllText("pub.cer"));
+                Console.WriteLine($"RSA公钥：\r\n{rsa.PublicKey}\r\n");
+                var encrypt = rsa.Encrypt(input);
+                Console.WriteLine($"RSA加密后内容：\r\n{encrypt}\r\n");
+                
+                rsa.CreateDecryptRSA(System.IO.File.ReadAllText("pri.pfx"));
+                Console.WriteLine($"RSA私钥：\r\n{rsa.PrivateKey}\r\n");
+                var decrypt = rsa.Decrypt(encrypt);
+                Console.WriteLine($"RSA解密后内容：\r\n{decrypt}\r\n");
+
+                // 私钥签名，公钥验签
+                var sign = rsa.Sign("SHA1", input);
+                Console.WriteLine($"RSA生成数字签名[SHAI]：\r\n{sign}\r\n");
+                string signResult = rsa.VerifySign(decrypt, "SHA1", sign) ? "验签通过" : "验签未通过";
+                Console.WriteLine($"RSA进行鉴别数字签名：{signResult}");
+            }
+
+            {
+                Console.WriteLine("-----------------------------------------------------通过读取PEM文件 RSA 文件加密与解密--------------------------------------------------");
+                var rsa = new github.RSA(System.IO.File.ReadAllText("pub.cer"),true);
+               rsa.Sign("SHA256", "SHA256");
             }
 
             {
